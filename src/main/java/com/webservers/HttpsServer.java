5c28f7c50331ed314aeba10743f83e99b63d1c8f
ThreaT <ThreaT>
2015-06-12 12:24:41 +0100

    WebServers

--- src/main/java/com/webservers/HttpsServer.java
+++ src/main/java/com/webservers/HttpsServer.java
@@ -17,6 +17,8 @@ import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.net.ssl.KeyManagerFactory;
@@ -32,12 +34,12 @@ public class HttpsServer {
     private static final Integer port = 1500;
     private static ByteBuffer inputBuffer = ByteBuffer.allocate(4096);
     private static ByteBuffer outputBuffer = ByteBuffer.allocate(4096);
-    private static final ByteBuffer networkBuffer = ByteBuffer.allocate(4096);
+    private static ByteBuffer networkBuffer = ByteBuffer.allocate(4096);
     private static Boolean handshakeComplete = false;
-    private static boolean initialHSComplete = false;
+    private static Boolean initialHSComplete = false;
     private static SSLEngineResult.HandshakeStatus initialHSStatus = SSLEngineResult.HandshakeStatus.NEED_UNWRAP;
-    private static SSLEngineResult result;
     private static final Charset ascii = Charset.forName("US-ASCII");
+    private static String request;
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
@@ -45,31 +47,44 @@ public class HttpsServer {
             final AsynchronousServerSocketChannel listener = AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(port));
             listener.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
                 @Override
-                public void completed(AsynchronousSocketChannel asynchronousSocketChannel, Void att) {
+                public void completed(AsynchronousSocketChannel asynchronousSocketChannel, Void attachment) {
                     try {
                         listener.accept(null, this);
-                        createSSLContext();
+                        //Configure SSL
+                        char[] password = "password".toCharArray();
+                        KeyStore keystore = KeyStore.getInstance("JKS");
+                        keystore.load(new FileInputStream("keystore.jks"), password);
+                        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+                        keyManagerFactory.init(keystore, password);
+                        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
+                        trustManagerFactory.init(keystore);
+                        sslContext = SSLContext.getInstance("TLS");
+                        sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
 
-                        //Do Handshake
-                        System.out.println("handshakeComplete: " + handshakeComplete);
+                        //Perform Handshake
+                        inputBuffer = ByteBuffer.allocate(4096);
+                        outputBuffer = ByteBuffer.allocate(4096);
+                        networkBuffer = ByteBuffer.allocate(4096);
+                        initialHSComplete = false;
+                        handshakeComplete = false;
+                        initialHSStatus = SSLEngineResult.HandshakeStatus.NEED_UNWRAP;
                         sslEngine = sslContext.createSSLEngine();
                         sslEngine.setUseClientMode(false);
                         outputBuffer = ByteBuffer.allocate(sslEngine.getSession().getPacketBufferSize());
-                        outputBuffer.position(0);
                         outputBuffer.limit(0);
                         inputBuffer = ByteBuffer.allocate(sslEngine.getSession().getPacketBufferSize());
                         while (!handshakeComplete) {
                             handshakeComplete = doHandshake(asynchronousSocketChannel);
                         }
 
-                        //Read Request
+                        //Request - Print request to console
                         read(asynchronousSocketChannel);
 
-                        //Send Response
-                        write(createResponse(), asynchronousSocketChannel);
+                        //Response - Print response to client (echo request to client)
+                        write(asynchronousSocketChannel);
 
                         asynchronousSocketChannel.close();
-                    } catch (InterruptedException | ExecutionException | IOException | CertificateException | KeyManagementException | KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException ex) {
+                    } catch (InterruptedException | ExecutionException | IOException | CertificateException | KeyManagementException | KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException | IllegalStateException | TimeoutException ex) {
                         Logger.getLogger(HttpsServer.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 }
@@ -90,38 +105,8 @@ public class HttpsServer {
         }
     }
 
-    private static void createSSLContext() throws CertificateException, IOException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, UnrecoverableKeyException {
-        char[] passphrase = "password".toCharArray();
-        KeyStore ks = KeyStore.getInstance("JKS");
-        ks.load(new FileInputStream("keystore.jks"), passphrase);
-        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-        kmf.init(ks, passphrase);
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-        tmf.init(ks);
-        sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
-    }
-
-    private static ByteBuffer createResponse() {
-        CharBuffer cb = CharBuffer.allocate(1024);
-        for (;;) {
-            try {
-                cb.put("HTTP/1.0 ").put("200 OK").put("\r\n");
-                cb.put("Server: niossl/0.1").put("\r\n");
-                cb.put("Content-type: ").put("text/plain; charset=iso-8859-1").put("\r\n");
-                cb.put("Content-length: ").put("31").put("\r\n");
-                cb.put("\r\n");
-                cb.put("bla bla bla bla");
-                break;
-            } catch (BufferOverflowException x) {
-                cb = CharBuffer.allocate(cb.capacity() * 2);
-            }
-        }
-        cb.flip();
-        return ascii.encode(cb);
-    }
-
     private static Boolean doHandshake(AsynchronousSocketChannel asynchronousSocketChannel) throws IOException, ExecutionException, InterruptedException, RuntimeException {
+        SSLEngineResult sslEngineResult;
         if (initialHSComplete) {
             return initialHSComplete;
         }
@@ -151,10 +136,10 @@ public class HttpsServer {
                     bb2.put(inputBuffer);
                     inputBuffer = bb2;
                     inputBuffer.flip();
-                    result = sslEngine.unwrap(inputBuffer, networkBuffer);
+                    sslEngineResult = sslEngine.unwrap(inputBuffer, networkBuffer);
                     inputBuffer.compact();
-                    initialHSStatus = result.getHandshakeStatus();
-                    switch (result.getStatus()) {
+                    initialHSStatus = sslEngineResult.getHandshakeStatus();
+                    switch (sslEngineResult.getStatus()) {
                         case OK:
                             switch (initialHSStatus) {
                                 case NOT_HANDSHAKING:
@@ -181,10 +166,10 @@ public class HttpsServer {
                 }
             case NEED_WRAP:
                 outputBuffer.clear();
-                result = sslEngine.wrap(ByteBuffer.allocate(0), outputBuffer);
+                sslEngineResult = sslEngine.wrap(ByteBuffer.allocate(0), outputBuffer);
                 outputBuffer.flip();
-                initialHSStatus = result.getHandshakeStatus();
-                switch (result.getStatus()) {
+                initialHSStatus = sslEngineResult.getHandshakeStatus();
+                switch (sslEngineResult.getStatus()) {
                     case OK:
                         if (initialHSStatus == SSLEngineResult.HandshakeStatus.NEED_TASK) {
                             Runnable runnable;
@@ -194,7 +179,7 @@ public class HttpsServer {
                             initialHSStatus = sslEngine.getHandshakeStatus();
                         }
                         if (initialHSComplete) {
-                            write(ByteBuffer.allocate(0), asynchronousSocketChannel);
+                            write(asynchronousSocketChannel);
                         }
                         break;
                 }
@@ -203,8 +188,8 @@ public class HttpsServer {
         return initialHSComplete;
     }
 
-    private static void read(AsynchronousSocketChannel asynchronousSocketChannel) throws IOException, ExecutionException, IllegalStateException, InterruptedException {
-        SSLEngineResult result2;
+    private static void read(AsynchronousSocketChannel asynchronousSocketChannel) throws IOException, ExecutionException, IllegalStateException, InterruptedException, TimeoutException {
+        SSLEngineResult sslEngineResult;
         if (asynchronousSocketChannel.read(inputBuffer).get() == -1) {
             sslEngine.closeInbound();
         }
@@ -214,9 +199,13 @@ public class HttpsServer {
             byteBuffer.put(inputBuffer);
             inputBuffer = byteBuffer;
             inputBuffer.flip();
-            result2 = sslEngine.unwrap(inputBuffer, networkBuffer);
+            sslEngineResult = sslEngine.unwrap(inputBuffer, networkBuffer);
+            asynchronousSocketChannel.read(inputBuffer).get(2000, TimeUnit.SECONDS);
+            inputBuffer.flip();
+            request = Charset.defaultCharset().decode(inputBuffer).toString();
+            System.out.println(request + "\n\n");
             inputBuffer.compact();
-            switch (result2.getStatus()) {
+            switch (sslEngineResult.getStatus()) {
                 case BUFFER_OVERFLOW:
                     break;
                 case BUFFER_UNDERFLOW:
@@ -228,7 +217,7 @@ public class HttpsServer {
                         break;
                     }
                 case OK:
-                    if (result2.getHandshakeStatus() == SSLEngineResult.HandshakeStatus.NEED_TASK) {
+                    if (sslEngineResult.getHandshakeStatus() == SSLEngineResult.HandshakeStatus.NEED_TASK) {
                         Runnable runnable;
                         while ((runnable = sslEngine.getDelegatedTask()) != null) {
                             runnable.run();
@@ -236,17 +225,33 @@ public class HttpsServer {
                     }
                     break;
             }
-        } while ((inputBuffer.position() != 0) && result2.getStatus() != SSLEngineResult.Status.BUFFER_UNDERFLOW);
+        } while ((inputBuffer.position() != 0) && sslEngineResult.getStatus() != SSLEngineResult.Status.BUFFER_UNDERFLOW);
     }
 
-    private static void write(ByteBuffer src, AsynchronousSocketChannel asynchronousSocketChannel) throws IOException {
+    private static void write(AsynchronousSocketChannel asynchronousSocketChannel) throws IOException {
         asynchronousSocketChannel.write(outputBuffer);
         outputBuffer.clear();
-        SSLEngineResult result2 = sslEngine.wrap(src, outputBuffer);
+        CharBuffer charBuffer = CharBuffer.allocate(1024);
+        for (;;) {
+            try {
+                charBuffer.put("HTTP/1.0 ").put("200 OK").put("\r\n");
+                charBuffer.put("Server: niossl/0.1").put("\r\n");
+                charBuffer.put("Content-type: ").put("text/html; charset=iso-8859-1").put("\r\n");
+                charBuffer.put("Content-length: ").put("31").put("\r\n");
+                charBuffer.put("\r\n");
+                charBuffer.put(request);
+                charBuffer.put("<html><head><title>HttpsServer</title></head><body><h3>HelloWorld!</h3></body></html>");
+                break;
+            } catch (BufferOverflowException x) {
+                charBuffer = CharBuffer.allocate(charBuffer.capacity() * 2);
+            }
+        }
+        charBuffer.flip();
+        SSLEngineResult sslEngineResult = sslEngine.wrap(ascii.encode(charBuffer), outputBuffer);
         outputBuffer.flip();
-        switch (result2.getStatus()) {
+        switch (sslEngineResult.getStatus()) {
             case OK:
-                if (result2.getHandshakeStatus() == SSLEngineResult.HandshakeStatus.NEED_TASK) {
+                if (sslEngineResult.getHandshakeStatus() == SSLEngineResult.HandshakeStatus.NEED_TASK) {
                     Runnable runnable;
                     while ((runnable = sslEngine.getDelegatedTask()) != null) {
                         runnable.run();
